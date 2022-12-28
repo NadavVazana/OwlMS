cd95e08462fe7f0f3856ccef4ee2b27cbf501736
Matan Dahan <ultravibez@hotmail.com>
2022-12-28 19:05:34 +0200

    fixed ts warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+// @ts-nocheck
 import { ThemeProvider } from "@emotion/react";
-import { Button } from "@mui/material";
 import * as React from "react";
-// @ts-ignore
 import { theme } from "./theme.ts";
+import { ItemPreview } from "./components/item-preview.tsx";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Button>Hello</Button>
+      <ItemPreview />
     </ThemeProvider>
   );
 };
