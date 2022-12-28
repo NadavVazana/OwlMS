0cade73247f22bdeed89525f4ce9547264ea0ffc
Nadav <nadavvivsa@gmail.com.com>
2022-12-28 16:57:52 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 
-export const App = ()=> {
-  
-  let getTime =  () =>{
-    return new Date().now()
-  }
+export const App = () => {
+
   return (
     <section className="App">
-      {getTime}
+
     </section>
   );
 }
-class test{
-  static name ='nadav'
-  lala = 'mama'
-  
-} 
 
-console.log(test.name);
-console.log(new test().lala)
