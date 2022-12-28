cc5a590fdc73b15bc923458956412fd213c34838
Nadav <nadavvivsa@gmail.com.com>
2022-12-28 18:45:05 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
+// @ts-ignore
+import { ItemCard } from './components/item-card.tsx';
 export const App = () => {
 
   return (
     <section className="App">
-        
+      <ItemCard />
+
     </section>
   );
 }
---
+++ src/components/item-card.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export const ItemCard = () =>{
+
+    return(
+        <section className="item-card">
+            
+        </section>
+    )
+}
