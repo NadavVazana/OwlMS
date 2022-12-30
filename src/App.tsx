d2a4cedf97b4925d28294d969642a77cccf2cc33
Matan Dahan <ultravibez@hotmail.com>
2022-12-30 18:04:03 +0200

    Removed tsx extensions from App Removed argument that's not needed anymore

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-// @ts-nocheck
 import { ThemeProvider } from "@emotion/react";
 import * as React from "react";
-import theme from "./theme.ts";
-import { ItemPreview } from "./components/item-preview.tsx";
-import { ItemCard } from "./components/item-card.tsx";
-import { ItemTitle } from "./components/item-title.tsx";
+import theme from "./theme";
+import { ItemPreview } from "./components/item-preview";
+import { ItemCard } from "./components/item-card";
+import { ItemTitle } from "./components/item-title";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <ItemCard>
-        <ItemTitle
-          title="Dark scroll for Overall Armor for INT 30%"
-          price="13,000,000"
-        />
+        <ItemTitle title="Dark scroll for Overall Armor for INT 30%" />
       </ItemCard>
       <ItemPreview />
     </ThemeProvider>
