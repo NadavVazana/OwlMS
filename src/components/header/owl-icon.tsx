3b0d9608f528f3c0b24b37f5400adfd501e5e44e
Nadav <nadavvivsa@gmail.com.com>
2023-01-03 18:59:56 +0200

    Home page finished

--- src/components/header/owl-icon.tsx
+++ src/components/header/owl-icon.tsx
@@ -3,9 +3,17 @@ import * as React from "react";
 import SvgIcon from "@mui/material/SvgIcon";
 import { ReactComponent as Owl } from "../../assets/images/Owl.svg";
 
-export const OwlIcon = () => {
+type OwlIconProps = {
+  fontSize: string;
+  isHeader: boolean;
+};
+
+export const OwlIcon = ({
+  fontSize = "40px",
+  isHeader = true,
+}: OwlIconProps) => {
   return (
-    <SvgIcon sx={{ fontSize: { md: "40px" } }}>
+    <SvgIcon sx={{ fontSize: isHeader ? { md: "40px" } : { xs: "80px" } }}>
       <Owl />
     </SvgIcon>
   );
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,29 @@
 // @ts-nocheck
+import { Box } from "@mui/material";
+import { Stack } from "@mui/system";
 import * as React from "react";
+import { Brand } from "../components/brand";
+import { SearchInput } from "../components/styled-components/search-input";
 
 export const HomePage = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <Box backgroundColor={"custom.background"}>
+      <Stack
+        gap={"40px"}
+        sx={{
+          transform: "translate(-50%,-50%)",
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+        }}
+      >
+        <Brand isHeader={false} fontSize="80px" />
+        <SearchInput
+          sx={{ width: { md: "800px", sm: "600px", xs: "350px" } }}
+          label={"Search example: Dark scroll for Overall armor..."}
+          height={"80px"}
+        />
+      </Stack>
+    </Box>
+  );
 };
